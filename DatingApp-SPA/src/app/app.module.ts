faa9478eab3a88cae133ca9f8bd8b706a009018f
ddarkeh <ddarkeh1@gmail.com>
2018-08-22 22:34:44 +0100

    add 3rd party lib, bootswatch, alertify, jwt helper

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,14 +1,17 @@
-import { ErrorInterceptorProvider } from './_services/error.interceptor';
-import { HomeComponent } from './home/home.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule} from '@angular/common/http';
 import { FormsModule} from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap';
 
+import { AlertifyService } from './_services/alertify.service';
+import { ErrorInterceptorProvider } from './_services/error.interceptor';
+import { HomeComponent } from './home/home.component';
 import { AuthService } from './_services/auth.service';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { RegisterComponent } from './register/register.component';
+
 
 @NgModule({
    declarations: [
@@ -20,11 +23,13 @@ import { RegisterComponent } from './register/register.component';
    imports: [
       BrowserModule,
       HttpClientModule,
-      FormsModule
+      FormsModule,
+      BsDropdownModule.forRoot()
    ],
    providers: [
        AuthService,
-       ErrorInterceptorProvider
+       ErrorInterceptorProvider,
+       AlertifyService
    ],
    bootstrap: [
       AppComponent
--- README.md
+++ README.md
@@ -2,10 +2,12 @@
 Project that implements a .net core 2.1 api and utilises Angular to consume it
 
 ## Getting Started
-npm -install to get angular dependancies, appsettings to modify to modify datasource/jwt token 
+- npm -install in DatingAPP-SPA to get angular dependancies 
+- appsettings to modify to modify datasource/jwt token 
+- Run dotnet ef update in the api directory to run migrations for the DB, do this after running application atleast one time
 
 ### Prerequisites
-Core 2.1, npm, angular 
+Core 2.1, npm, angular6
 
 ## Built With
 ASP Core 2.1 - API Framework
