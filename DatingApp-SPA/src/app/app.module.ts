0e7f54f3fc571f9a3b58e85f5deebd0a4ef881eb
Steven Mitchell <ddarkeh1@gmail.com>
2018-08-22 17:02:40 +0100

    added a error interceptor into angular

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { ErrorInterceptorProvider } from './_services/error.interceptor';
 import { HomeComponent } from './home/home.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -22,7 +23,8 @@ import { RegisterComponent } from './register/register.component';
       FormsModule
    ],
    providers: [
-       AuthService
+       AuthService,
+       ErrorInterceptorProvider
    ],
    bootstrap: [
       AppComponent
